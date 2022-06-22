dfb420aedc934baecb5617e73eaded538159cfb0
minimum106 <99700099+minimum106@users.noreply.github.com>
2022-06-22 17:26:08 +0900

    実装前の準備が完了

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from "react";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button></button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>みかんを買う</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>リンゴを買う</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
   );
 }
