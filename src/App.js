e2705f5b3d8cd5b9ac28208cb9ada172a4f6acf6
minimum106 <99700099+minimum106@users.noreply.github.com>
2022-06-23 11:55:50 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -28,20 +28,50 @@ export default function App() {
    * 未完了パート
    */
 
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "リンゴを買う",
-    "みかんを買う"
-  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+
+  //削除ボタンを謳歌したら自身をuseStatから削除
+  const onClickDelete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    //第一引数の要素から、第二要素分を削除
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
+  //完了ボタンを押下したら自身をincompleteTodosのuseStateから削除して
+  //completeTodosのuseStateに追加
+  const onClickComplete = (index) => {
+    //未完了から削除
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    //完了に追加
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    //再描画
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
 
   /**
    * 完了パート
    */
 
-  const [completeTodos, setCompleteTodos] = useState([
-    "バナナを買う",
-    "マンゴーを買う"
-  ]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
+  const onClickBack = (index) => {
+    //完了から削除
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    //未完了に追加
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    //再描画
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
+  /**
+   * HTMLパート
+   */
   return (
     <>
       <div className="input-area">
@@ -55,12 +85,13 @@ export default function App() {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                {/*アロー関数にしないとonClickDeleteが複数回実行されてまう*/}
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
@@ -69,11 +100,11 @@ export default function App() {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
